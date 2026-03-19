d4f323ee87c4dbdc82e831d1493accf14519e72d
ธนวัฒน์ อินตายวง <eak9599@gmail.com>
2026-03-19 09:48:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8109059113-c4e1d",
+  "appId": "1:1078575742644:web:5828f0a832d7d97613e1e1",
+  "apiKey": "AIzaSyDkRxpF-8lR69_gagvnLWQ-LSOL8whx36A",
+  "authDomain": "studio-8109059113-c4e1d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1078575742644"
+};
